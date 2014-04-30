8bb41bc67fa475c67cef0ba11f41575503977248
nawakkap <p.pakkawan@gmail.com>
2014-04-30 11:51:06 +0700

    Redeploy to localhost

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,37 @@
 
 ActiveRecord::Schema.define(:version => 20130404083048) do
 
+  create_table "finishgoods", :force => true do |t|
+    t.string   "displaydate"
+    t.integer  "unita"
+    t.integer  "unita2"
+    t.integer  "unitb"
+    t.integer  "unitc"
+    t.integer  "unitd"
+    t.date     "procdate"
+    t.string   "extprogcode"
+    t.string   "prodname"
+    t.string   "mach"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "production_details", :force => true do |t|
+    t.integer  "production_reason_id"
+    t.integer  "time"
+    t.string   "production_ukey"
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
+  end
+
+  create_table "production_reasons", :force => true do |t|
+    t.string   "mc_id"
+    t.integer  "seq"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
   create_table "productiondetails", :force => true do |t|
     t.integer  "productionreason_id"
     t.integer  "time"
@@ -38,6 +69,30 @@ ActiveRecord::Schema.define(:version => 20130404083048) do
     t.datetime "updated_at",  :null => false
     t.integer  "downtime"
     t.integer  "scrap"
+  end
+
+  create_table "products", :force => true do |t|
+    t.string   "displayid"
+    t.string   "nameeng"
+    t.string   "nameth"
+    t.string   "nameinit"
+    t.string   "size"
+    t.string   "thmin"
+    t.string   "threp"
+    t.string   "film"
+    t.float    "estw"
+    t.float    "wmin"
+    t.float    "wmax"
+    t.float    "acctw"
+    t.integer  "pcspack"
+    t.integer  "pcstruck"
+    t.float    "wagekg"
+    t.string   "wdisplay"
+    t.string   "inprod"
+    t.float    "pct"
+    t.string   "vat"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "staffs", :force => true do |t|
