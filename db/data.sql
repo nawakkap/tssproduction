28da286a1f2f81b406a1dc41ebc32b96f5ba49a5
nawakkap <p.pakkawan@gmail.com>
2014-04-30 14:54:01 +0700

    Config db/seed.rb&data.sql

---
+++ db/data.sql
@@ -0,0 +1,30 @@
+/*!40101 SET NAMES utf8 */;
+
+insert into `staffs` (`id`, `name`, `position`, `created_at`, `updated_at`) values('1','ธีรวุฒิ','FR','2014-04-30 13:25:24','2014-04-30 13:25:26');
+insert into `staffs` (`id`, `name`, `position`, `created_at`, `updated_at`) values('2','กรุง','FR','2014-04-30 13:25:24','2014-04-30 13:25:26');
+insert into `staffs` (`id`, `name`, `position`, `created_at`, `updated_at`) values('3','สมภาร','FR','2014-04-30 13:25:24','2014-04-30 13:25:26');
+insert into `staffs` (`id`, `name`, `position`, `created_at`, `updated_at`) values('4','เจี๊ยบ','FR','2014-04-30 13:25:24','2014-04-30 13:25:26');
+insert into `staffs` (`id`, `name`, `position`, `created_at`, `updated_at`) values('5','ชาญณรงค์','FR','2014-04-30 13:25:24','2014-04-30 13:25:26');
+insert into `staffs` (`id`, `name`, `position`, `created_at`, `updated_at`) values('6','สนอง','FR','2014-04-30 13:25:24','2014-04-30 13:25:26');
+insert into `staffs` (`id`, `name`, `position`, `created_at`, `updated_at`) values('7','สำเริง','FR','2014-04-30 13:25:24','2014-04-30 13:25:26');
+insert into `staffs` (`id`, `name`, `position`, `created_at`, `updated_at`) values('8','จำเริญ','FR','2014-04-30 13:25:24','2014-04-30 13:25:26');
+
+
+/*
+SQLyog Ultimate v9.31 GA
+MySQL - 5.0.51b-community-nt-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `productionreasons` (`id`, `mc_id`, `seq`, `description`, `created_at`, `updated_at`) values('1',NULL,'1','ปกติ','2014-04-30 13:21:22','2014-04-30 13:21:26');
+insert into `productionreasons` (`id`, `mc_id`, `seq`, `description`, `created_at`, `updated_at`) values('2',NULL,'2','เปลี่ยนลูกรีด','2014-04-30 13:21:22','2014-04-30 13:21:26');
+insert into `productionreasons` (`id`, `mc_id`, `seq`, `description`, `created_at`, `updated_at`) values('3',NULL,'3','ปรับโรล','2014-04-30 13:21:22','2014-04-30 13:21:26');
+insert into `productionreasons` (`id`, `mc_id`, `seq`, `description`, `created_at`, `updated_at`) values('4',NULL,'4','รอฟิล์ม','2014-04-30 13:22:19','2014-04-30 13:22:22');
+insert into `productionreasons` (`id`, `mc_id`, `seq`, `description`, `created_at`, `updated_at`) values('5',NULL,'5','รอคำสั่งผลิต','2014-04-30 13:22:31','2014-04-30 13:22:34');
+insert into `productionreasons` (`id`, `mc_id`, `seq`, `description`, `created_at`, `updated_at`) values('6',NULL,'6','รอขึ้นของ','2014-04-30 13:22:19','2014-04-30 13:22:22');
+insert into `productionreasons` (`id`, `mc_id`, `seq`, `description`, `created_at`, `updated_at`) values('7',NULL,'7','เครนเสีย','2014-04-30 13:22:19','2014-04-30 13:22:22');
+insert into `productionreasons` (`id`, `mc_id`, `seq`, `description`, `created_at`, `updated_at`) values('8',NULL,'8','เครื่องเสีย','2014-04-30 13:22:19','2014-04-30 13:22:22');
+insert into `productionreasons` (`id`, `mc_id`, `seq`, `description`, `created_at`, `updated_at`) values('9',NULL,'9','ซ่อมบำรุง','2014-04-30 13:22:19','2014-04-30 13:22:22');
+insert into `productionreasons` (`id`, `mc_id`, `seq`, `description`, `created_at`, `updated_at`) values('10',NULL,'10','เปลี่ยนใบเลื่อย','2014-04-30 13:22:19','2014-04-30 13:22:22');
+insert into `productionreasons` (`id`, `mc_id`, `seq`, `description`, `created_at`, `updated_at`) values('11',NULL,'11','อื่นๆ','2014-04-30 13:22:19','2014-04-30 13:22:22');
